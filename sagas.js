0415ffc75d278f23fcd8296c2e8b503958775963
Fabio Matias <fabio.matias@talkdesk.com>
2018-11-27 16:47:33 +0000

    Add new increment async saga
    The new saga still doesn't start with the saga middleware.

--- sagas.js
+++ sagas.js
@@ -1,3 +1,16 @@
+import { delay } from 'redux-saga'
+import { put, takeEvery } from 'redux-saga/effects'
+
 export function* helloSaga() {
   console.log('Hello Sagas!')
+}
+
+// async increment task
+export function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: 'INCREMENT' })
+}
+
+export function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
